cef9d26760b26d75b282d004da6c6c985a43aeac
nader-dab <nader.dabour@abv.bg>
2013-09-18 15:23:13 +0300

    added error notifier

--- BgTatkoForum/NewThread.aspx.designer.cs
+++ BgTatkoForum/NewThread.aspx.designer.cs
@@ -56,5 +56,14 @@ namespace BgTatkoForum {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton LinkButtonSaveThread;
+        
+        /// <summary>
+        /// RequiredFieldValidatorThreadTitle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidatorThreadTitle;
     }
 }
